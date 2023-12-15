f38c125cc7aee4149be4b88179abe4795abba26e
shamil <shamilmohd418@gmail.com>
2023-12-15 14:52:49 +0530

    first cmt

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+  <BrowserRouter>
+  <Navbar/>
+  <Routes>
+    {/* <Route path='/' Component={Navbar}/> */}
+  </Routes>
+  </BrowserRouter>
+    </>
+  )
+}
+
+export default App
