62e2f7eec18dfa22db34aedf61a68515a609dee4
shamil <shamilmohd418@gmail.com>
2023-12-18 17:00:54 +0530

    ghg

---
+++ middleware/multer.js
@@ -0,0 +1,17 @@
+import multer from "multer"
+
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'./images')
+    },
+    filename:(req,file,cb)=>{
+        cb(null,file.fieldname+'.jpg')
+    }
+})
+const upload=multer({
+    storage:storage
+})
+
+
+
+export default upload;
