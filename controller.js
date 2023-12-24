2978af2970ae93dcc0873aa4fd960600501a9b16
shamil <shamilmohd418@gmail.com>
2023-12-24 16:35:43 +0530

    dfdf

--- controller.js
+++ controller.js
@@ -170,29 +170,81 @@ export async function getCategoryWisedProduct(req, res) {
   }
 }
 
-export async function AddCustomer(req,res){
+export async function AddCustomer(req, res) {
   try {
-      const {name,email,phone,password,personal_address,state,district,pincode,place,landmark,street,photo}=req.body;
-      console.log(name,email,phone,password,personal_address,state,district,pincode,place,landmark,street,photo);
-      if(!(name&&email&&phone&&password&&personal_address&&state&&district&&pincode&&place&&landmark,street))
-      return res.status(404).send("fields are empty")
-  
-      bcrypt.hash(password,10)    
-      .then((hashedPwd)=>{
-          admin_schema.create({name,email,phone,password:hashedPwd,personal_address,state,district,pincode,place,landmark,street,photo});
-      })
-      .then(()=>{
-          res.status(201).send("sucessfully registered")
-      })
-    .catch((error)=>{
-      res.status(500).send(error)
-     })
-      
-     } catch (error) {
-      console.log(error);
-  
+    const {
+      name,
+      email,
+      phone,
+      password,
+      personal_address,
+      state,
+      district,
+      pincode,
+      place,
+      landmark,
+      street,
+      photo
+    } = req.body;
+
+    console.log(
+      name,
+      email,
+      phone,
+      password,
+      personal_address,
+       state,
+      district,
+      pincode,
+      place,
+      landmark,
+      street
+      photo
+    );
+
+    if (
+      !(
+        name &&
+        email &&
+        phone &&
+        password &&
+        personal_address &&
+        state &&
+        district &&
+        pincode &&
+        place &&
+        landmark &&
+        street
+      )
+    )
+      return res.status(404).send("Fields are empty");
+
+    const hashedPwd = await bcrypt.hash(password, 10);
+
+    const newCustomer = {
+      name,
+      email,
+      phone,
+      password: hashedPwd,
+      personal_address,
+      location: {
+        state,
+        district,
+        pincode,
+        place,
+        landmark,
+        street
+      },
+      photo
+    };
+
+    const customer = await admin_schema.create(newCustomer);
+
+    res.status(201).send("Successfully registered");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
   }
-  
 }
 
 
