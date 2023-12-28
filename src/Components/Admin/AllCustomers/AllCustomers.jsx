2f64358dc00416c0a5388a24669945af5fbfc642
shamil <shamilmohd418@gmail.com>
2023-12-28 11:50:59 +0530

    asas

--- src/Components/Admin/AllCustomers/AllCustomers.jsx
+++ src/Components/Admin/AllCustomers/AllCustomers.jsx
@@ -4,7 +4,14 @@ import './AllCustomers.scss'
 const AllCustomers = () => {
   return (
     <div className='allCustomersMain'>
-      <div className="main"></div>
+      <div className="main">
+        <div className="dp-div">
+          <div className="dp"></div>
+        </div>
+        <div className="name-div">
+          <h4>Mohammed Shamil</h4>
+        </div>
+      </div>
     </div>
   )
 }
