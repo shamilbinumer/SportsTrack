2789ec5a5b99e156a652355badd7236cecc60d99
shamil <shamilmohd418@gmail.com>
2023-12-29 10:21:13 +0530

    sdsa

--- src/Components/Customer/Cart/Cart.jsx
+++ src/Components/Customer/Cart/Cart.jsx
@@ -23,6 +23,16 @@ const Cart = () => {
               <div className="details-details-section">
                 <p className="item-name">ITEM</p>
                 <p className="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                <select name="" id="">
+                  <option value="1">Qty : 1</option>
+                  <option value="2">Qty : 2</option>
+                  <option value="3">Qty : 3</option>
+                  <option value="4">Qty : 4</option>
+                  <option value="5">Qty : 5</option>
+                  <option value="6">Qty : 6</option>
+                  <option value="7">Qty : 7</option>
+                  <option value="8">Qty : 8</option>
+                </select>
               </div>
             </div>
         </div>
