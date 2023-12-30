b09603b5983f45858fdd3254caca21c1c046fa39
shamil <shamilmohd418@gmail.com>
2023-12-30 15:05:21 +0530

    asdasd

--- src/Components/Customer/Cart/Cart.jsx
+++ src/Components/Customer/Cart/Cart.jsx
@@ -92,6 +92,7 @@ const Cart = () => {
             {getPrdct.length === 0 ? (
                    <>
                     <p className="no-items-message">No items in the cart</p>
+                    <div className='shpDiv'><Link className='shp-now-btn' to='/'>Shop Now</Link></div>
                    </>
                 ) : (
                     <>
