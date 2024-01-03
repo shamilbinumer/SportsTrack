c78edff5dc7170e64292acd1b1881347a72f2cf7
shamil <shamilmohd418@gmail.com>
2024-01-03 14:03:47 +0530

    sfsdf

--- Models/cart.model.js
+++ Models/cart.model.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 const cart_schema=new mongoose.Schema({
     cust_id:{type:String},
+    prod_id:{type:String},
     product_name:{type:String},
     category:{type:String},
     description:{type:String},
     price:{type:String},
     size:{type:String},
+    quantity:{type:String},
     banner:{type:String}
 })
 
