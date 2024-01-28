03ece4543a1bee88c8568c7b52970d5a820bc8ee
shamil <shamilmohd418@gmail.com>
2024-01-28 11:29:31 +0530

    sdsad

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 async function connection(){
-    const URL=process.env.DB_URL+process.env.DB_NAME
+    const URL=process.env.atles_url+process.env.DB_NAME
     const db=await mongoose.connect(URL);
     console.log("Database connected","as",process.env.DB_NAME);
     return db
